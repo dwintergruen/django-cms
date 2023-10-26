a08d637170c1e52d54c396afb4edb079bd378aa8
wintergruen <dirk.wintergruen@klassik-stiftung.de>
2023-10-26 10:04:56 +0200

    change django-cms to cms

--- setup.py
+++ setup.py
@@ -2,4 +2,21 @@
 from setuptools import setup
 
 
-setup()
+
+setup(
+    name='cms',
+    version=__version__,
+    author='Django CMS Association and contributors',
+    author_email='info@django-cms.org',
+    url='https://www.django-cms.org/',
+    license='BSD-3-Clause',
+    description='Lean enterprise content management powered by Django.',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    packages=find_packages(exclude=['project', 'project.*']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=REQUIREMENTS,
+    classifiers=CLASSIFIERS,
+    test_suite='runtests.main',
+)
+
